8f6ed8706ecbe2f4e40c80ccba21d70ab5dcf5ba
dbratus <dmitry.bratus@gmail.com>
2012-04-11 23:08:18 +0400

    Command line flags; Loading from zip.

--- src/cmd/impack.go
+++ src/cmd/impack.go
@@ -7,21 +7,43 @@ import (
 	"image"
 	"image/draw"
 	"image/png"
+	"flag"
+	"io"
 )
 
+var archiveFileName *string = flag.String("a", "", "Archive file name.")
+var imageDirName *string = flag.String("d", "", "Image directory name.")
+var outputFileName *string = flag.String("o", "", "Output file name.")
+
 func main() {
-	if len(os.Args) < 3 {
-		fmt.Println("Usage: impack <images-dir> <output-png-file>")
-		return
-	}
+	flag.Parse()
 
 	var images []image.Image
-
-	if pathChan, err := getImagePaths(os.Args[1]); err == nil {
+	
+	if len(*archiveFileName) == 0 {
+		var err os.Error
+		var pathChan chan string
+		
+		if len(*imageDirName) > 0 {
+			if pathChan, err = getImagePaths(*imageDirName); err != nil {
+				fmt.Errorf("%s\n", err)
+				return
+			}
+		} else {
+			pathChan = make(chan string, 10)
+			go func() {
+				var line string
+				for _, err := fmt.Scan(&line); err == nil; _, err = fmt.Scan(&line) {
+					pathChan <- line
+				}
+				
+				pathChan <- ""
+			}()
+		}
+		
 		images = loadImages(pathChan)
 	} else {
-		fmt.Errorf("%s\n", err)
-		return
+		images = loadImagesFromZip(*archiveFileName)
 	}
 
 	rects := make([]image.Rectangle, len(images))
@@ -38,10 +60,18 @@ func main() {
 		draw.Draw(dest, rects[i], images[i], image.Pt(0, 0), draw.Src)
 	}
 	
-	if out, err := os.Create(os.Args[2]); err == nil {
-		png.Encode(out, dest)
+	var out io.Writer
+	
+	if len(*outputFileName) > 0 {
+		if f, err := os.Create(*outputFileName); err == nil {
+			out = f
+		} else {
+			fmt.Errorf("%s\n", err)
+			return
+		}
 	} else {
-		fmt.Errorf("%s\n", err)
-		return
+		out = os.Stdout
 	}
+	
+	png.Encode(out, dest)
 }
--- src/cmd/loading.go
+++ src/cmd/loading.go
@@ -8,12 +8,19 @@ import (
 	"strings"
 	"os"
 	"fmt"
+	"archive/zip"
+	"io"
 )
 
 const nParallelLoaders = 4
 
+type loaderRequest struct {
+	name   string
+	reader io.ReadCloser
+}
+
 func loadImages(pathChan chan string) []image.Image {
-	loadingIn := make(chan string, nParallelLoaders)
+	loadingIn := make(chan loaderRequest, nParallelLoaders)
 	loadingOut := make(chan image.Image, nParallelLoaders)
 	stopChan := make(chan int)
 
@@ -22,64 +29,119 @@ func loadImages(pathChan chan string) []image.Image {
 	for i := 0; i < nParallelLoaders; i++ {
 		go loader(loadingIn, loadingOut, stopChan)
 	}
-	
+
 	cnt := 0
 	for pth := <-pathChan; ; pth = <-pathChan {
 		end := pth == ""
 
 		if !end {
-			loadingIn <- pth
-			cnt++
+			if fl, err := os.Open(pth); err == nil {
+				loadingIn <- loaderRequest{pth, fl}
+				cnt++
+			} else {
+				fmt.Errorf("%s\n", err)
+			}
 		}
 
 		if cnt == nParallelLoaders || end {
-			for ; cnt > 0; cnt-- {
-				if img := <-loadingOut; img != nil {
-					images = append(images, img)
-				}
-			}
+			getImagesFromChan(loadingOut, &images, &cnt)
 
 			if end {
 				break
 			}
 		}
 	}
-	
+
 	for i := 0; i < nParallelLoaders; i++ {
 		stopChan <- 1
 	}
-	
+
 	return images
 }
 
-func loader(in chan string, out chan image.Image, stop chan int) {
+func loadImagesFromZip(archPath string) []image.Image {
+	var reader *zip.ReadCloser
+
+	if r, err := zip.OpenReader(archPath); err == nil {
+		reader = r
+		defer reader.Close()
+	} else {
+		fmt.Printf("%s\n", err)
+		return make([]image.Image, 0)
+	}
+
+	loadingIn := make(chan loaderRequest, nParallelLoaders)
+	loadingOut := make(chan image.Image, nParallelLoaders)
+	stopChan := make(chan int)
+	
+	images := make([]image.Image, 0, 100)
+
+	for i := 0; i < nParallelLoaders; i++ {
+		go loader(loadingIn, loadingOut, stopChan)
+	}
+
+	cnt := 0
+	for _, f := range reader.File {
+		var isPng, isJpg bool
+
+		if m, err := path.Match("*.png", f.Name); err == nil {
+			isPng = m
+		}
+
+		if m, err := path.Match("*.jpg", f.Name); err == nil {
+			isJpg = m
+		}
+
+		if !strings.HasPrefix(f.Name, "__MACOSX") && (isPng || isJpg) {
+			if r, err := f.Open(); err == nil {
+				loadingIn <- loaderRequest{f.Name, r}
+				cnt++
+			} else {
+				fmt.Printf("%s\n", err)
+			}
+		}
+		
+		if cnt == nParallelLoaders {
+			getImagesFromChan(loadingOut, &images, &cnt)
+		}
+	}
+	
+	getImagesFromChan(loadingOut, &images, &cnt)
+
+	return images
+}
+
+func getImagesFromChan(loadingOut chan image.Image, images *[]image.Image, cnt *int) {
+	for ; *cnt > 0; *cnt-- {
+		if img := <-loadingOut; img != nil {
+			*images = append(*images, img)
+		}
+	}
+}
+
+func loader(in chan loaderRequest, out chan image.Image, stop chan int) {
 	for {
 		select {
-		case pth := <-in:
-			if fl, err := os.Open(pth); err == nil {
-				switch strings.ToLower(path.Ext(pth)) {
-				case ".jpg":
-					if img, err := jpeg.Decode(fl); err == nil {
-						out <- img
-					} else {
-						fmt.Errorf("%s\n", err)
-						out <- nil
-					}
-				case ".png":
-					if img, err := png.Decode(fl); err == nil {
-						out <- img
-					} else {
-						fmt.Errorf("%s\n", err)
-						out <- nil
-					}
-				}
-
-				if err = fl.Close(); err != nil {
+		case req := <-in:
+			switch strings.ToLower(path.Ext(req.name)) {
+			case ".jpg":
+				if img, err := jpeg.Decode(req.reader); err == nil {
+					out <- img
+				} else {
 					fmt.Errorf("%s\n", err)
+					out <- nil
 				}
-			} else {
+			case ".png":
+				if img, err := png.Decode(req.reader); err == nil {
+					out <- img
+				} else {
+					fmt.Errorf("%s\n", err)
+					out <- nil
+				}
+			}
+
+			if err := req.reader.Close(); err != nil {
 				fmt.Errorf("%s\n", err)
-				out <- nil
 			}
 		case <-stop:
 			return
